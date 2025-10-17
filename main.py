41641f3761aa9d635039fc269019554e562cae08
artumont <197291181+artumont@users.noreply.github.com>
2025-10-16 22:19:25 -0600

    feat: add constants and GameState class for arena, deck, and elixir management

--- main.py
+++ main.py
@@ -9,6 +9,33 @@ if __name__ == "__main__":
     while True:
         raw = device.get_frame()
         frame = cv2.cvtColor(np.array(raw), cv2.COLOR_RGB2BGR)
+        # arena tiles
+        cv2.rectangle(
+            frame,
+            (int(22), int(62)),
+            (int(52 + 16 * 18), int(296 + 12.6 * 15)),
+            color=(0, 255, 0),
+            thickness=2,
+        )
+        
+        # cards
+        cv2.rectangle(
+            frame,
+            (int(72), int(535)),
+            (int(72 + 72 * 4), int(520 + 104)),
+            color=(255, 0, 255),
+            thickness=2,
+        )
+        
+        # elixir
+        cv2.rectangle(
+            frame,
+            (int(72), int(625)),
+            (int(72 + 28.8 * 10), int(625 + 20)),
+            color=(0, 255, 255),
+            thickness=2,
+        )
+        
         cv2.imshow("Android Screen", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
